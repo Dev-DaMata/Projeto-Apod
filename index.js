54e2f7cc9c7d3c07866f572a17bb6615e8eb5017
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-05-10 16:19:55 -0300

    comentei o projeto

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-    $(".video").hide()
+    $(".video").hide() // ele esconde o  tag de video (.hide)
     $('#btn').click(function () { //esse é a achão do botão 
         var data = $('#data').val(); // ele esta pegando a data que o usuario selecionou e add na api 
         $.ajax({ // o começo da api
             url: `https:api.nasa.gov/planetary/apod?api_key=ZrXw96z5JVLotfMMIt7IZYsjjwFgjEJzD2tLzhdg&date=${data}`, // add a url da api + a key(chave) &date = ${data}
             success: function (data) { //se a api der certo "success"  
                 console.log(data);
-                $(".titulo").text(data.title)
-                $(".imagem").attr("src", data.url)
-                $(".descri").text(data.explanation)
-                if (data.media_type == 'video') {
-                    $(".video").attr("src", data.url).show()
-                    $(".imagem").hide()
-                } else if (data.media_type == 'image') {
-                    $(".imagem").attr("src", data.url).show()
-                    $(".video").hide()
+                $(".titulo").text(data.title)//estou chamando o id do titulo, .text significa texto
+                $(".imagem").attr("src", data.url)//estou chamando o id da img, attr(significa atributo)
+                $(".descri").text(data.explanation)//estou chamando o id do descri, text significa texto
+                if (data.media_type == 'video') { //se o media_type for video
+                    $(".video").attr("src", data.url).show()// vai aparecer a tag de video(.show())
+                    $(".imagem").hide()//e esconde a tag (.hide())
+                } else if (data.media_type == 'image') { // se o media_type for imagem
+                    $(".imagem").attr("src", data.url).show()// vai aparecer a tag de imagem(.show())
+                    $(".video").hide()//e esconde a tag (.hide())
                 }
 
 
@@ -31,6 +31,6 @@
         if (saida.midia_Type == 'image') { // se tiver uma imagem ela deve imprimir no html
             imagem.html(`<img class="img" src="${saida.url}"/> `) // ele esta pegando a imagem do api da nasa e jogando no meu html
         } else {
-            imagem.html(`<iframe class="img" src="${saida.url}?autoplay=1&mute=1"></iframe>`)
+            imagem.html(`<iframe class="img" src="${saida.url}?autoplay=1&mute=1"></iframe>`)//se não for ele da um auto play 
         }
     }
